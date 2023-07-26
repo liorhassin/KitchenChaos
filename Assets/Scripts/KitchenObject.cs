1eebcd6fab895821dee69675d2da29bd2cad193c
Lior Hassin <liorhassin3@gmail.com>
2023-07-26 11:47:29 +0300

    KitchenObjectUpdate
    Added support to know where KitchenObject is and basic test to move it from one counter to the other.

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -6,8 +6,35 @@ public class KitchenObject : MonoBehaviour {
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+    private ClearCounter clearCounter;
+
     public KitchenObjectSO GetKitchenObjectSO() {
         return kitchenObjectSO;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter) {
+        
+        //Clear KitchenObject from old counter.
+        if (this.clearCounter != null) {
+            this.clearCounter.ClearKitchenObject();
+        }
+        
+        //Add KitchenObject to new counter.
+        this.clearCounter = clearCounter;
+        
+        if (clearCounter.HasKitchenObject()) {
+            Debug.LogError("Counter already has a KitchenObject!");
+        }
+        
+        clearCounter.SetKitchenObject(this);
+        
+        //Update visuals.
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter() {
+        return clearCounter;
     }
 
 }
